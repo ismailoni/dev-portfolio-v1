2aa71d1b267b5a90941aad21bbdbc9ec27e44174
Ismail Oni <ismailoni80@gmail.com>
2024-11-10 11:02:47 +0100

    Initilize services page

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+'use client'
+
+import {BsArrowDownRight} from 'react-icons/bs'
+import Link from 'next/link'
+import { motion } from 'framer-motion'
+
+const services = [
+  {
+    num: '01',
+    title: 'Web Development',
+    Description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem non tempore',
+    href: ''
+  },
+  {
+    num: '02',
+    title: 'Web Hosting',
+    Description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem non tempore',
+    href: ''
+  },
+  {
+    num: '03',
+    title: 'Logo Design',
+    Description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem non tempore',
+    href: ''
+  },
+]
 
 const Services = () => {
+
   return (
-    <div>Services Page</div>
+    <section className='min-h-[80vh] flex flex-col justify-center py-12 xl:py-0'>
+      <div className="container mx-auto">
+        <motion.div className='grid grid-cols-1 md:grid-cols-2 gap-16' initial={{opacity: 0}} animate={{opacity: 1, transition: {delay: 2.4, duration: 0.4, ease: 'easeIn'}}}>
+          {services.map((service, i) => {
+            return (
+              <div key={i} className='flex-1 flex flex-col justify-center gap-6 group'>
+                {/* top  */}
+                <div className='w-full flex justify-between items-center'>
+                  <div className='text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover group-hover:animate-bounce transition-all duration-500'>{service.num}</div>
+                  <Link href={service.href} className='w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45'>
+                    <BsArrowDownRight className='text-primary text-3xl'/>
+                  </Link>
+                </div>
+                {/* title  */}
+                <h2 className='text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500'>{service.title}</h2>
+                {/* Description */}
+                <p className='text-white/60'>{service.Description}</p>
+
+                {/* border */}
+                <div className='border-b border-white/20 w-full'></div>
+              </div>
+            )
+          })}
+        </motion.div>
+      </div>
+    </section>
   )
 }
 
